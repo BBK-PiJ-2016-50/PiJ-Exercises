8aaf1582c9484a48d96de55f33c5f3b0fa392742
IanRobinson01 <ianrobinson01@hotmail.com>
2016-10-21 23:37:41 +0100

    added validation methods for same suit, 4 of a kind and pair

--- Week 1/task11.groovy
+++ Week 1/task11.groovy
@@ -1,14 +1,16 @@
-//tell user what is best hand based on those cards
-//8 types of hand to check
-
 println "Enter 5 cards (rank and suit) and see what you got!"
 
 int cardCounter = 1
-String card1 = ""
-String card2 = ""
-String card3 = ""
-String card4 = ""
-String card5 = ""
+String card1rank = 0
+String card1suit = ""
+String card2rank = 0
+String card2suit = ""
+String card3rank = 0
+String card3suit = ""
+String card4rank = 0
+String card4suit = ""
+String card5rank = 0
+String card5suit = ""
 
 //get rank and suit for 5 cards
 while (cardCounter <= 5) {
@@ -18,16 +20,28 @@ while (cardCounter <= 5) {
 		print "Enter card " + cardCounter + " rank: "
 		String rank = System.console().readLine()
 		if (rank == 'A' || rank == '2' || rank == '3' || rank == '4' || rank == '5' || rank == '6' || rank == '7' || rank == '8' || rank == '9' || rank == '10' || rank == 'J' || rank == 'Q' || rank == 'K') {
+			//convert lettered ranks to numbers for easier validation of hand
+			if (rank == 'A') {
+				rank = '1'
+			} else if (rank == 'J') {
+				rank = '11'
+			} else if (rank == 'Q') {
+				rank = '12'
+			} else if (rank == 'K') {
+				rank = '13'
+			}
+			
+			//assign rank to correct card number
 			if (cardCounter == 1) {
-				card1 = card1 + rank
+				card1rank = Integer.parseInt(rank)
 			} else if (cardCounter == 2) {
-				card2 = card2 + rank
+				card2rank = Integer.parseInt(rank)
 			} else if (cardCounter == 3) {
-				card3 = card3 + rank
+				card3rank = Integer.parseInt(rank)
 			} else if (cardCounter == 4) {
-				card4 = card4 + rank
+				card4rank = Integer.parseInt(rank)
 			} else {
-				card5 = card5 + rank
+				card5rank = Integer.parseInt(rank)
 			}
 			correctRank = true
 		} else {
@@ -42,15 +56,15 @@ while (cardCounter <= 5) {
 		String suit = System.console().readLine()
 		if (suit == 'H' || suit == 'D' || suit == 'C' || suit == 'S') {
 			if (cardCounter == 1) {
-				card1 = card1 + suit
+				card1suit = card1suit + suit
 			} else if (cardCounter == 2) {
-				card2 = card2 + suit
+				card2suit = card2suit + suit
 			} else if (cardCounter == 3) {
-				card3 = card3 + suit
+				card3suit = card3suit + suit
 			} else if (cardCounter == 4) {
-				card4 = card4 + suit
+				card4suit = card4suit + suit
 			} else {
-				card5 = card5 + suit
+				card5suit = card5suit + suit
 			}
 			correctSuit = true
 		} else {
@@ -60,4 +74,37 @@ while (cardCounter <= 5) {
 	cardCounter++
 }
 
-print card1 + " " + card2 + " " + card3 + " " + card4 + " " + card5
+//check for same suit required in straight flush and flush
+boolean sameSuit = false
+if (card1suit == card2suit && card1suit == card3suit && card1suit == card4suit && card1suit == card5suit) {
+	sameSuit = true
+}
+
+//check for 4 cards
+def rankList = [card1rank, card2rank, card3rank, card4rank, card5rank]
+boolean rankCountFour = false
+if (rankList.count{it == card1rank} == 4 || rankList.count{it == card2rank} == 4) {
+	rankCountFour = true
+}
+
+//check for same rank - 2 cards
+boolean rankCountTwo
+if (rankList.count{it == card1rank} == 2 || rankList.count{it == card2rank} == 2 || rankList.count{it == card3rank} == 2 || rankList.count{it == card4rank} == 2) {
+	rankCountTwo = true
+}
+
+//check for same full house
+//check for two pair
+
+//check for consectutive cards
+
+//check hands
+//check for straight flush - same suit and consecutive ranks
+//check for poker - 4 of 5 card have same rank
+//check for full house - 3 of a kind and 2 of a kind
+//check for flush - all same suit, not consecutive
+//check for straight - consecutive rank but not same suit
+//check for three of a kind - 3 cards have same rank
+//check for two pairs - see pair
+//check for a pair - two of the cards have the same rank
+//else nothing
