bf6a96587fc157e13e5e5dd94e29f654b150e1f8
IanRobinson01 <ianrobinson01@hotmail.com>
2016-10-22 17:31:01 +0100

    added answer to week 1 task 1 question

---
+++ Week 1/task1.groovy
@@ -0,0 +1,11 @@
+//takes your birth year and subtracts from current year to find your age
+//it then checks if your age is greater than 17 and outputs the result
+
+
+int currentYear = 2012
+print "When were you born (year)?"
+String str = System.console().readLine();
+int birthYear = Integer.parseInt(str);
+int ageAprox = currentYear - birthYear;
+String result = (ageAprox > 17) ? "" : "not "
+println "It seems you are " + result + "an adult."
