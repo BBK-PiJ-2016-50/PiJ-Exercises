fd8ac5e08a5f3b92328e3e6ca8ba20574dc3b026
IanRobinson01 <ianrobinson01@hotmail.com>
2016-10-22 12:45:10 +0100

    added solution to second part of week 3 task 9, i.e. counting letter specified by user

--- Week 3/task9.groovy
+++ Week 3/task9.groovy
@@ -1,11 +1,24 @@
-//read text and count how many e's there are
 print "Enter some text: "
-String str = System.console().readLine()
-int letterE = 0
-for (count = 0; count < str.length(); count++) {
-	String currentLetter = str.substring(count, count + 1)
-	if (currentLetter == 'e') {
-		letterE ++
+String sentence = System.console().readLine()
+print "Enter a letter to count: "
+String letter = System.console().readLine()
+int letterCount = 0
+for (count=0; count < sentence.length(); count++) {
+	String currentLetter = sentence.substring(count, count + 1)
+	if (currentLetter == letter) {
+		letterCount++
 	}
 }
-println "There are " + letterE + " e's in this sentence"
+println "There are " + letterCount + " " + letter + "'s in this sentence"
+
+//solution for counting e's in sentence
+//print "Enter some text: "
+//String str = System.console().readLine()
+//int letterE = 0
+//for (count=0; count < str.length(); count++) {
+//	String currentLetter = str.substring(count, count + 1)
+//	if (currentLetter == 'e') {
+//		letterE ++
+//	}
+//}
+//println "There are " + letterE + " e's in this sentence"
