0a817dc44d96d95f2e659fce3749d8cbc3796964
IanRobinson01 <ianrobinson01@hotmail.com>
2016-10-22 17:19:43 +0100

    first attempt at solution to week 3 task 16.  Needs more work

---
+++ Week 3/task16.groovy
@@ -0,0 +1,37 @@
+//not completely happy with this solution
+
+print "Enter a number with comma's a decimal dots: "
+String str = System.console().readLine()
+
+// remove dot and comma and store dot's place
+String pointsRemoved = ""
+int decPoints = 0
+for (count=0; count < str.length(); count++) {
+	String current = str.substring(count, count + 1)
+	if (current != "," && current != ".") {
+		pointsRemoved = pointsRemoved + current
+	}
+	if (current == ".") {
+		String vals = str.substring(count + 1)
+		decPoints = vals.length()
+	}
+}
+
+//halve the resulting number
+int num = Integer.parseInt(pointsRemoved)
+int halfNum = num / 2
+String numNoDec = String.valueOf(halfNum)
+
+String finalNum = ""
+
+//add the decimal back into the string at correct num of digits from right
+for (count=0; count < numNoDec.length(); count++) {
+	String current = numNoDec.substring(count, count + 1)
+	if ((numNoDec.length() - decPoints) == count) {
+		finalNum = finalNum + "." + current
+	} else {
+		finalNum = finalNum + current
+	}
+}
+
+println "Half " + str + " is " + finalNum
