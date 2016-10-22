52952d1b499b912bea0800da186bbf6b3f8961d0
IanRobinson01 <ianrobinson01@hotmail.com>
2016-10-22 11:23:54 +0100

    added solution to second part of week 3 task 8, i.e. print one word per line

--- Week 3/task8.groovy
+++ Week 3/task8.groovy
@@ -1,6 +1,16 @@
 print "Enter some text: "
 String str = System.console().readLine()
-
 for (count=0; count < str.length(); count++) {
-	println str.substring(count, count + 1)
+	String currentLetter = str.substring(count, count + 1)
+	if (currentLetter != " "){
+		print currentLetter
+	} else {
+		println ""
+	}
 }
+
+//solution for reading one letter per line
+//String str = System.console().readLine()
+//for (count=0; count < str.length(); count++) {
+//	println str.substring(count, count + 1)
+//}
