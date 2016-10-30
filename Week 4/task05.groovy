8d93b0510cb8cf9635cbebd40532fe1ffeda538c
IanRobinson01 <ianrobinson01@hotmail.com>
2016-10-30 11:05:39 +0000

    completed solution for week 4 task 5

--- Week 4/task05.groovy
+++ Week 4/task05.groovy
@@ -3,27 +3,39 @@ class Point {
 	double y;
 	
 	double distanceTo(Point point) {
-		//calculates the distance to another point.
+		this.x = x
+		this.y = y
+		return Math.sqrt(((this.x - this.y)**2) + ((point.x - point.y)**2))
 	}
 	
 	double distanceToOrigin() {
-		//calculates the distance to the origin
-		//Implement it by calling the first method.
+		Point origin = new Point()
+		origin.x = 0
+		origin.y = 0
+		return distanceTo(origin)
 	}
 	
 	void moveTo(double x, double y){
-		//changes the coordinates of this point to be the given parameters x and y
+		this.x = x
+		this.y = y
 	}
 	
 	void moveTo(Point point) {
-		//changes the coordinates of this point to move where the given point is
+		this.x = point.x
+		this.y = point.y
 	}
 	
 	Point clone() {
-		//returns a copy of the current point with the same coordinates
+		Point clone = new Point()
+		clone.x = this.x
+		clone.y = this.y
+		return clone
 	}
 	
 	Point opposite() {
-		//returns a copy of the current point with the coordinates multiplied by −1
+		Point opp = clone()
+		opp.x = opp.x * -1
+		opp.y = opp.y * -1
+		return opp
 	}
 }
