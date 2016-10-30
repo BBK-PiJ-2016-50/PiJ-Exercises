ac05eadc9d48c1e65945e9bfe5540cae8978d51c
IanRobinson01 <ianrobinson01@hotmail.com>
2016-10-30 22:04:58 +0000

    added solution for week 1 exercise 18

---
+++ Week 1/task18.groovy
@@ -0,0 +1,34 @@
+//ask user for number n
+//calculates the partial addition of n terms of this infinite sum
+//how many terms required to get first 3 digits correct (3.14)
+//how many terms required to get first 10 digits correct (3.14159265358)
+
+print "Enter a number: "
+String str = System.console().readLine()
+int num = Integer.parseInt(str)
+
+int counter = 0
+int divisor = 1
+double total = 0
+boolean plusSign = true
+
+while (counter < num) {
+	if (plusSign) {
+		double calc = 4 / divisor
+		total += calc
+		divisor += 2
+		plusSign = false
+		counter++
+	} else {
+		double calc = 4 / divisor
+		total -= calc
+		divisor += 2
+		plusSign = true
+		counter++
+	}
+}
+
+println "The output is: " + total
+
+//623 to get it to output correctly to 3 digits
+//1000000  to get 6 digits
