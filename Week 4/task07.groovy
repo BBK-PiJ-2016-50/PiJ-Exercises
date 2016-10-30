9cd47672aba53bf2c758878ca973dc222f1593d2
IanRobinson01 <ianrobinson01@hotmail.com>
2016-10-30 11:49:28 +0000

    initial solution for week 4 task 7

---
+++ Week 4/task07.groovy
@@ -0,0 +1,33 @@
+//calculate how much must be paid at the end
+//t = c*(1+(r/100))
+//t is total, c is cost, r is rate(%)
+//print total amount to be paid
+//print money to be paid every year
+//print number of years before interest is paid and initial capital remains
+//method for each
+
+print "What is the total amount borrowed for your mortgage? "
+String mtgStr = System.console().readLine()
+double mtg = Double.parseDouble(mtgStr)
+print "How many years to pay it back? "
+String yrsStr = System.console().readLine()
+double yrs = Double.parseDouble(yrsStr)
+print "What is the interest rate? "
+String rateStr = System.console().readLine()
+double rate = Double.parseDouble(rateStr)
+
+double totalAmount(double mtg, double rate) {
+	double total = mtg*(1+(rate/100))
+	return total
+}
+
+double payPerYear(double total, double yrs) {
+	double perYear = total / yrs
+	return perYear
+}
+
+double interestYears(double total, double mtg, double yrs) {
+	double interest = total - mtg
+	return payPerYear(interest, yrs)
+}
+
