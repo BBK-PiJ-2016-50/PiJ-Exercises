9020dc5fc0ed8a3103d1d2ca781b1fa4d28ee1dc
IanRobinson01 <ianrobinson01@hotmail.com>
2016-11-01 20:44:17 +0000

    added logic to test the ArrayChecker class

--- Week 5/task02.groovy
+++ Week 5/task02.groovy
@@ -1,7 +1,10 @@
-int[] intArray = [1,2,5,6,7,2,1];
+int[] intArray = [1,2,3,6,6,3,2,1];
 
 ArrayChecker ac = new ArrayChecker();
-if (!ac.isSymmetrical(intArray)) {
-	
+
+if (ac.isSymmetrical(intArray)) {
+	println "Array is symmetrical";
+} else {
+	print "Array is not symmetrical.  So the array is reversed: ";
+	println ac.reverse(intArray);
 }
-print ac.isSymmetrical(intArray);
