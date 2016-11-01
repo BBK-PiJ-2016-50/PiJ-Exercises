b97383189141821f158a8092036831050335bcc2
IanRobinson01 <ianrobinson01@hotmail.com>
2016-11-01 20:45:05 +0000

    completed method for checking array symmetry.  Added method to reverse array

--- Week 5/ArrayChecker.java
+++ Week 5/ArrayChecker.java
@@ -1,28 +1,26 @@
 public class ArrayChecker {
 	
 	public boolean isSymmetrical(int[] x) {
-		//returns true if array of int is symmetrical, false otherwise
-		int firstIndex = 0;
-		int lastIndex = 1;
-		boolean symmetrical = false;
-		double halfx = (x.length) / 2;
-		System.out.println(halfx);
-		while (firstIndex < halfx) {
-			if (x[firstIndex] == x[x.length-lastIndex]) {
-				System.out.println(x[firstIndex]);
-				System.out.println(x[x.length-lastIndex]);
-				firstIndex++;
-				lastIndex--;
-				symmetrical = true;	
-			} else {
-				symmetrical = false;
+		//returns true if array of integers is symmetrical, false otherwise
+		//x is the array of ints
+		int arrayLength = x.length;
+		for (int i = 0; i < arrayLength / 2; i++) {
+			if (x[i] != x[arrayLength - 1 - i]) {
+				return false;
 			}
 		}
-		return symmetrical;
+		return true;
 	}
 	
-	//public int[] reverse(int[] x) {
-		//returns another array of int but reversed
-	//}
+	public int[] reverse(int[] x) {
+		//returns another array of integers but reversed
+		//x is the array of ints
+		int arrayLength = x.length;
+		int[] reversedArray = new int[arrayLength];
+		for (int i = 0; i < arrayLength; i++) {
+			reversedArray[arrayLength - 1 - i] = x[i];
+		}
+		return reversedArray;
+	}
 	
 }
