06eb1e0ca067c32b2eaeab249d167e11b7e44951
IanRobinson01 <ianrobinson01@hotmail.com>
2016-11-06 17:56:16 +0000

    implemented remaining methods for matrix class for week 5 task 4

--- Week 5/Matrix.java
+++ Week 5/Matrix.java
@@ -1,42 +1,81 @@
 public class Matrix {
 	
 	private int[][] matrix;
+	private int rows;
+	private int cols;
 	
 	public Matrix(int rows, int cols) {
+		this.rows = rows - 1;
+		this.cols = cols - 1;
 		matrix = new int[rows][cols];
 		for (int i = 0; i < rows; i++) {
 			for (int j = 0; j < cols; j++) {
 				this.matrix[i][j] = 1;
 			}
-			
 		}		
 	}
 	
-	//public void setElement(int pos1, int pos2, int newVal) {
-		//check indices are valid
-		//modify the element of the array
-		//if invalid method does nothing
-	//}
+	public void setElement(int row, int col, int newVal) {
+		if ((row <= this.rows && row >= 0) || (col <= this.cols && col >= 0)) {
+			matrix[row][col] = newVal;
+		} else {
+			System.out.println("invalid indeces");
+		}
+	}
 	
-	//public void setRow(int arrayRow, String numList) {
-		//check index is valid and nums are correct (i.e. if aray has 3 columns the string contains 3 numbers)
-		//modify one whole row of array
-		//if invalid do nothing
-	//}
+	public void setRow(int row, String numList) {
+		int[] nums = stringToArray(numList);
+		if ((row <= this.rows && row >= 0) && (nums.length - 1 == this.cols)) {
+			for (int i = 0; i <= nums.length - 1; i++) {
+				matrix[row][i] = nums[i];
+			}
+		} else {
+			System.out.println("Either invalid row index or incorrect amount of values for columns entered");
+		}
+	}
 	
-	//public void setColumn(int arrayCol, String numList) {
-		//check index is valid and nums are correct (i.e. if aray has 3 columns the string contains 3 numbers)
-		//modify one whole column of array
-		//if invalid do nothing
-	//}
+	public void setColumn(int col, String numList) {
+		int[] nums = stringToArray(numList);
+		if ((col <= this.cols && col >= 0) && (nums.length - 1 == this.rows)) {
+			for (int i = 0; i <= nums.length - 1; i++) {
+				matrix[i][col] = nums[i];
+			}
+		} else {
+			System.out.println("Either invalid column index or incorrect amount of values for rows entered");
+		}
+	}
 	
-	//private String toString() {
-		//returns values of array as a string "[1,2,3]"
-	//}
+	private int[] stringToArray(String arrayStr) {
+		String[] strNums = arrayStr.split(",");
+		int[] nums = new int[strNums.length];
+		for (int i = 0; i <= nums.length - 1; i++) {
+			nums[i] = Integer.parseInt(strNums[i]);
+		}
+		return nums;
+	}
+
+	public String toString() {
+		String strArray = "[";
+		for (int i = 0; i <= rows; i++) {
+			for (int j = 0; j <= cols; j++) {
+				strArray += matrix[i][j];
+			}
+			if (i != rows) {
+				strArray += ";";
+			}
+		}
+		strArray += "]";
+		return strArray;
+	}
 	
-	//public void prettyPrint() {
-		//prints values of the matrix in legible format
-	//}
+	public void prettyPrint() {
+		for (int i = 0; i <= rows; i++) {
+			for (int j = 0; j <= cols; j++) {
+				System.out.print(matrix[i][j] + "\t");
+			}
+			System.out.println("");
+		}
+	}
 	
 }
 
