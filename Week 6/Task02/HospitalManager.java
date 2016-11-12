6f988fc96f22dc53f65b11d6ed3cd6a770e325c5
IanRobinson01 <ianrobinson01@hotmail.com>
2016-11-12 09:07:28 +0000

    Added method for returning patient list length and replaced print method while loop with a do while loop

---
+++ Week 6/Task02/HospitalManager.java
@@ -0,0 +1,75 @@
+public class HospitalManager {
+
+	private Patient firstPatient = null;
+	private Patient current= null;
+	
+	public void addPatient(Patient newPatient) {
+		if (firstPatient == null) {
+			firstPatient = newPatient;
+			return;
+		}
+		
+		current = this.firstPatient;
+		while (current.getNextPatient() != null) {
+			current = current.getNextPatient();
+		}
+		current.setNextPatient(newPatient);
+	}
+	
+	public boolean deletePatient(String name) {
+		boolean done = false;
+		if (firstPatient == null) {
+			System.out.println("No Records to Delete");
+		}
+		
+		if (firstPatient.getName().equals(name)) {
+			if (firstPatient.getNextPatient() == null) {
+				firstPatient = null;
+			} else {
+				firstPatient = firstPatient.getNextPatient();
+			} 
+			done = true;
+		}
+		
+		Patient current = firstPatient;
+		
+		while (current.getNextPatient() != null) {
+			if (current.getNextPatient().getName().equals(name)){
+				current.setNextPatient(current.getNextPatient().getNextPatient());
+				return true;
+			}
+			current = current.getNextPatient();
+		}
+		return done;
+	}
+	
+	public void printRecords(){
+		if (firstPatient == null) {
+			System.out.println("No Records to Print!");
+			return;
+		}
+		
+		Patient current = firstPatient;
+		
+		do {
+			System.out.println ("Name: " + current.getName() + "\t Age: " + current.getAge() + "\t Illness: "
+			+ current.getIllness());
+			current = current.getNextPatient();
+		} while (current != null); 
+	}
+	
+	public int patientListLength(){
+		int counter = 0;
+		if (firstPatient == null) {
+			return counter;
+		} 
+		Patient current = firstPatient;
+		do {
+			counter++;
+			current = current.getNextPatient();
+		} while (current != null);
+		return counter;
+
+	}
+
+}
---
+++ Week 6/Task02/OurMain.java
@@ -0,0 +1,37 @@
+public class OurMain {
+
+	public static void main(String[] args) {
+		
+		HospitalManager hospital = new HospitalManager();
+		
+		Patient one = new Patient("One",15,"ABC1");
+		Patient two = new Patient("Two",16,"ABC2");
+		Patient three = new Patient("Three",17,"ABC3");
+		Patient four = new Patient("Four",18,"ABC4");
+		Patient five = new Patient("Five",19,"ABC5");
+		Patient six = new Patient("Six",15,"ABC6");
+		Patient seven = new Patient("Seven",16,"ABC2");
+		Patient eight = new Patient("Eight",17,"ABC3");
+		Patient nine = new Patient("Nine",18,"ABC4");
+		Patient ten = new Patient("Ten",19,"ABC5");
+		
+		hospital.addPatient(one);
+		hospital.addPatient(two);
+		hospital.addPatient(three);
+		hospital.addPatient(four);
+		hospital.addPatient(five);
+		hospital.addPatient(six);
+		hospital.addPatient(seven);
+		hospital.addPatient(eight);
+		hospital.addPatient(nine);
+		hospital.addPatient(ten);
+		hospital.printRecords();
+		System.out.println("\nPatient List Length: " + hospital.patientListLength() + "\n");
+		
+		hospital.deletePatient("Four");
+		hospital.deletePatient("One");
+		hospital.printRecords();
+		System.out.println("\nPatient List Length: " + hospital.patientListLength() + "\n");
+	}
+	
+}
