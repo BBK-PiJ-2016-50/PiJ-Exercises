2d04f758ab91195799a413937840230837b3f54a
IanRobinson01 <ianrobinson01@hotmail.com>
2016-11-15 20:33:00 +0000

    updated launcher so it calls static method

--- Week 6/Task07/Launcher.java
+++ Week 6/Task07/Launcher.java
@@ -2,8 +2,7 @@ public class Launcher {
 	
 	public static void main(String[] args) {
 		int[] intArray = {1, 2, 3, 4, 5};
-		ArrayUtilities myUtil = new ArrayUtilities();
-		myUtil.array2list(intArray);
+		ArrayUtilities.array2list(intArray);
 	}
 	
 }
