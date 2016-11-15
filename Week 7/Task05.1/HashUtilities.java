baa6938c936398836aa96dfc555354b0d4fa8e8f
IanRobinson01 <ianrobinson01@hotmail.com>
2016-11-15 21:36:07 +0000

    solution for week 7 task 5.1

--- Week 7/Task05.1/HashUtilities.java
+++ Week 7/Task05.1/HashUtilities.java
@@ -1,7 +1,7 @@
 public class HashUtilities {
 
 	public static int shortHash(int myInt) {
-		int newInt = Math.abs(myInt); 
+		int newInt = Math.abs(myInt % 1000); 
 		return newInt;
 	}
 
--- Week 7/Task05.1/Launcher.java
+++ Week 7/Task05.1/Launcher.java
@@ -1,7 +1,11 @@
 public class Launcher {
 	
 	public static void main(String[] args) {
-		System.out.println(HashUtilities.shortHash(445));
+		System.out.println("Give me a string and I will calculate its hash code");
+		String str = System.console().readLine();
+		int hash = str.hashCode();
+		int smallHash = HashUtilities.shortHash(hash);
+		System.out.println("0 < " + smallHash + " < 1000");
 	}
 	
 }
