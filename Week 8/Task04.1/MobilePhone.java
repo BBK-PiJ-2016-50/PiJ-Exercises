eef1a62aa5da2b3dd0c7903710cd99db29bd34cd
Mr Ian Robinson <irobin02@dcs.bbk.ac.uk>
2016-11-21 20:43:00 +0000

    adding solution to week 8 Task 4.1

---
+++ Week 8/Task04.1/MobilePhone.java
@@ -0,0 +1,51 @@
+public class MobilePhone extends OldPhone {
+	
+	private String[] lastTenNumbers = new String[10];
+	private int length = lastTenNumbers.length;
+	
+	public MobilePhone(String brand) {
+		super(brand);		
+	}
+	
+	@Override
+	public void call(String number) {
+		super.call(number);
+		for (int i = 0; i < length; i++) {
+			if (i == 9 && lastTenNumbers[i] != null) {
+				lastTenNumbers[0] = lastTenNumbers[1];
+				lastTenNumbers[1] = lastTenNumbers[2];
+				lastTenNumbers[2] = lastTenNumbers[3];
+				lastTenNumbers[3] = lastTenNumbers[4];
+				lastTenNumbers[4] = lastTenNumbers[5];
+				lastTenNumbers[5] = lastTenNumbers[6];
+				lastTenNumbers[6] = lastTenNumbers[7];
+				lastTenNumbers[7] = lastTenNumbers[8];
+				lastTenNumbers[8] = lastTenNumbers[9];
+				lastTenNumbers[9] = number;
+			}
+			if (lastTenNumbers[i] == null) {
+				lastTenNumbers[i] = number;
+				return;
+			}
+		}
+		
+	}
+	
+	public void ringAlarm(String alarm) {
+		System.out.println("Alarm ringing: " + alarm);
+	}
+	
+	private void playGame(String game) {
+		System.out.println("Playing game: " + game);
+	}
+	
+	public void printLastNumbers() {
+		System.out.println("Last 10 numbers called:");	
+		for (int i = 0; i < length; i++) {
+			if (lastTenNumbers[i] != null) {
+				System.out.println(lastTenNumbers[i]);
+			}
+		}
+	}
+	
+}
---
+++ Week 8/Task04.1/SmartPhone.java
@@ -0,0 +1,29 @@
+public class SmartPhone extends MobilePhone {
+	
+	public SmartPhone(String brand) {
+		super(brand);		
+	}
+	
+	@Override
+	public void call(String number) {
+		if (number.startsWith("00")){
+			browseWeb(number);
+		} else {
+			super.call(number);
+		}
+	}
+	
+	public void browseWeb(String search) {
+		System.out.println("Search query: " + search);
+	}
+	
+	public String findPosition() {
+		String gpsPosition = "123456789";
+		return gpsPosition;
+	}
+	
+	public void playGame(String game) {
+		System.out.println("Playing game: " + game);
+	}
+	
+}
