5adffc5fb49a59049a2426b7411044cccfe9b769
IanRobinson01 <ianrobinson01@hotmail.com>
2016-11-21 22:42:55 +0000

    added explanation to RestrictedSmartPhone class

--- Week 8/Task04.2/PhoneLauncher.java
+++ Week 8/Task04.2/PhoneLauncher.java
@@ -6,27 +6,28 @@ public class PhoneLauncher {
 	}
 	
 	public void launch() {
-		
-		SmartPhone smartPhone = new SmartPhone("Nokia");
-		System.out.println(smartPhone.getBrand());
-		smartPhone.call("1");
-		smartPhone.call("002");
-		smartPhone.call("3");
-		smartPhone.call("4");
-		smartPhone.call("5");
-		smartPhone.call("6");
-		smartPhone.call("7");
-		smartPhone.call("8");
-		smartPhone.call("9");
-		smartPhone.call("10");
-		smartPhone.call("0011");
-		smartPhone.call("12");
-		smartPhone.call("13");
-		smartPhone.ringAlarm("ring ring");
-		smartPhone.playGame("rubbish game");
-		smartPhone.printLastNumbers();
-		smartPhone.browseWeb("browsing for food");
-		System.out.println("GPS Position: " + smartPhone.findPosition());
+		RestrictedSmartPhone restSmartPhone = new RestrictedSmartPhone("Samsung");
+		restSmartPhone.playGame("rubbish game");
+		//SmartPhone smartPhone = new SmartPhone("Nokia");
+		//System.out.println(smartPhone.getBrand());
+		//smartPhone.call("1");
+		//smartPhone.call("002");
+		//smartPhone.call("3");
+		//smartPhone.call("4");
+		//smartPhone.call("5");
+		//smartPhone.call("6");
+		//smartPhone.call("7");
+		//smartPhone.call("8");
+		//smartPhone.call("9");
+		//smartPhone.call("10");
+		//smartPhone.call("0011");
+		//smartPhone.call("12");
+		//smartPhone.call("13");
+		//smartPhone.ringAlarm("ring ring");
+		//smartPhone.playGame("rubbish game");
+		//smartPhone.printLastNumbers();
+		//smartPhone.browseWeb("browsing for food");
+		//System.out.println("GPS Position: " + smartPhone.findPosition());
 	}
 	
 }
--- Week 8/Task04.2/RestrictedSmartPhone.java
+++ Week 8/Task04.2/RestrictedSmartPhone.java
@@ -1,8 +1,17 @@
 public class RestrictedSmartPhone extends SmartPhone {
 	
+	public RestrictedSmartPhone(String brand) {
+		super(brand);		
+	}
+	
 	@Override
-	private void playGame(String game) {
-		super.playGame(game)
+	public void playGame(String game) {
+		System.out.println("You do not have access to this");
 	}
 	
 }
+
+//it doesn't look like this is possible because you can't override 
+//public methods to make them more private
+//what i have done is to override the inherited method 
+//and make the game playing options unavailable
