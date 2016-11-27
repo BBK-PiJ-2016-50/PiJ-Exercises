d7bdecd6955874427e4ca0f46757e4af96ec4f96
IanRobinson01 <ianrobinson01@hotmail.com>
2016-11-27 14:13:48 +0000

    completed solution to week 7 task05.3.  Not sure if this is the intended implementation but appears to work

--- Week 7/Task05.3/ImplementsSimpleMap.java
+++ Week 7/Task05.3/ImplementsSimpleMap.java
@@ -4,19 +4,76 @@ public class ImplementsSimpleMap implements SimpleMap {
 	private Node current = null;
 	
 	public String put(int key, String name) {
-		//code
+		//same key can be used by more than one Node
+		
+		//create new node
+		Node newNode = new Node(key, name);
+		
+		//check if map empty.  if it is than add as first node
+		if (isEmpty()) {
+			first = newNode;
+		} else {
+			//if not empty then add to end of map
+			current = first;
+			while (current.getNextNode() != null) {
+				current = current.getNextNode();
+			}
+			current.setNextNode(newNode);
+		}
+		
+		return "";
 	}
 	
 	public String[] get(int key) {
-		//code
+		
+		//check if list is empty
+		if (isEmpty()) {
+			return null;
+		} 
+		String[] result = new String[10]; //array to hold values of matching key
+		current = first;
+		if (current.getNextNode() == null) {
+			if (current.getKey() == key) {
+				result[0] = current.getValue();
+			}
+		} else {
+			current = first;
+			int i = 0; //counter so value can be added to empty slot in array
+			while (current.getNextNode() != null) {
+				if (current.getNextNode().getKey() == key) {
+					result[i] = current.getNextNode().getValue();
+					i++;
+				}
+				current = current.getNextNode();
+			}
+		}
+		return result;
 	}
 	
 	public void remove(int key, String name) {
-		//code
+		//check if list is empty
+		if (isEmpty()) {
+			return;
+		}
+		//check if key, value exists.  If it does then remove it
+		current = first;
+		while (current.getNextNode() != null) {
+			if (current.getNextNode().getKey() == key) {
+				if (current.getNextNode().getValue() == name) {
+					current.setNextNode(current.getNextNode().getNextNode());
+				}
+			}
+			current = current.getNextNode();
+		}
+		//if not found return null
+		return;
 	}
 	
 	public boolean isEmpty() {
-		//code
+		if (first == null) {
+			return true;
+		}
+		return false;
 	}
 	
 }
--- Week 7/Task05.3/Launcher.java
+++ Week 7/Task05.3/Launcher.java
@@ -10,8 +10,25 @@ public class Launcher {
 		performMap(myMap);
 	}
 	
-	private void performMap() {
-		//map code here
+	private void performMap(SimpleMap map) {
+		map.put(10, "ten");
+		map.put(2, "two");
+		map.put(3, "three");
+		map.put(3, "different value");
+		printValues(map.get(3));
+		map.remove(2, "two");
+		map.remove(3, "three");
+		printValues(map.get(3));
+		System.out.println(map.isEmpty());
+	}
+	
+	private void printValues(String[] stringArray) {
+		String[] values = stringArray;
+		for (int i = 0; i < values.length; i++) {
+			if (values[i] != null) {
+				System.out.println(values[i]);
+			}
+		}
 	}
 	
 }
