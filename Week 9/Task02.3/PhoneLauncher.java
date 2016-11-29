ff7f5fe4d1f9b2144def0538f1df57fe085fdce1
IanRobinson01 <ianrobinson01@hotmail.com>
2016-11-29 20:51:35 +0000

    added solution to week 9 task 2.3

---
+++ Week 9/Task02.3/PhoneLauncher.java
@@ -0,0 +1,24 @@
+public class PhoneLauncher {
+	
+	public static void main(String[] args) {
+		PhoneLauncher launcher = new PhoneLauncher();
+		launcher.launch();
+	}
+	
+	private void launch() {
+		MobilePhone myPhone = new SmartPhone();
+		testPhone(myPhone); // passing myPhone object to new method
+	}
+	
+	private void testPhone(Phone phone) {
+		phone.call("1");
+		//phone.ringAlarm("ring ring");
+		//phone.playGame("rubbish game");
+		//phone.printLastNumbers();
+		//phone.browseWeb("browsing for food");
+		//System.out.println("GPS Position: " + phone.findPosition());
+	}
+	
+}
+
+//the testPhone method can only call the method declared in the Phone interface
