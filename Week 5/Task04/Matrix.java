d41e8f6de9a68bdfa5e71de4cf8e5a55a63891b0
IanRobinson01 <ianrobinson01@hotmail.com>
2016-12-09 20:22:20 +0000

    restructured week 5 folders so seach task has own sub directory

---
+++ Week 5/Task04/Matrix.java
@@ -0,0 +1,93 @@
+public class Matrix {
+	
+	private int[][] matrix;
+	private int rows;
+	private int cols;
+	
+	public Matrix(int rows, int cols) {
+		this.rows = rows - 1;
+		this.cols = cols - 1;
+		matrix = new int[rows][cols];
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				this.matrix[i][j] = 1;
+			}
+		}		
+	}
+	
+	public void setElement(int row, int col, int newVal) {
+		if ((row <= this.rows && row >= 0) || (col <= this.cols && col >= 0)) {
+			matrix[row][col] = newVal;
+		} else {
+			System.out.println("invalid indeces");
+		}
+	}
+	
+	public void setRow(int row, String numList) {
+		int[] nums = stringToArray(numList);
+		if ((row <= this.rows && row >= 0) && (nums.length - 1 == this.cols)) {
+			for (int i = 0; i <= nums.length - 1; i++) {
+				matrix[row][i] = nums[i];
+			}
+		} else {
+			System.out.println("Either invalid row index or incorrect amount of values for columns entered");
+		}
+	}
+	
+	public void setColumn(int col, String numList) {
+		int[] nums = stringToArray(numList);
+		if ((col <= this.cols && col >= 0) && (nums.length - 1 == this.rows)) {
+			for (int i = 0; i <= nums.length - 1; i++) {
+				matrix[i][col] = nums[i];
+			}
+		} else {
+			System.out.println("Either invalid column index or incorrect amount of values for rows entered");
+		}
+	}
+	
+	private int[] stringToArray(String arrayStr) {
+		String[] strNums = arrayStr.split(",");
+		int[] nums = new int[strNums.length];
+		for (int i = 0; i <= nums.length - 1; i++) {
+			nums[i] = Integer.parseInt(strNums[i]);
+		}
+		return nums;
+	}
+
+	public String toString() {
+		String strArray = "[";
+		for (int i = 0; i <= rows; i++) {
+			for (int j = 0; j <= cols; j++) {
+				strArray += matrix[i][j];
+			}
+			if (i != rows) {
+				strArray += ";";
+			}
+		}
+		strArray += "]";
+		return strArray;
+	}
+	
+	public void prettyPrint() {
+		for (int i = 0; i <= rows; i++) {
+			for (int j = 0; j <= cols; j++) {
+				System.out.print(matrix[i][j] + "\t");
+			}
+			System.out.println("");
+		}
+	}
+	
+	public boolean isSymmetrical() {
+		MatrixChecker mc = new MatrixChecker();
+		boolean isSym = mc.isSymmetrical2D(matrix);
+		return isSym;
+	}
+	
+	public boolean isTriangular() {
+		MatrixChecker mc = new MatrixChecker();
+		boolean isTri = mc.isTriangular(matrix);
+		return isTri;
+	}
+	
+}
+
