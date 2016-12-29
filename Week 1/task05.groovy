d953242505d4c5a0c1acb569cad8a7f704b2fed0
IanRobinson01 <ianrobinson01@hotmail.com>
2016-12-29 17:25:40 +0000

    added comments to week 1 task05

--- Week 1/task05.groovy
+++ Week 1/task05.groovy
@@ -1,3 +1,4 @@
+//ask user for two numbers to multiply
 println "Enter two numbers to find the product"
 print "Enter the first number: "
 String str = System.console().readLine()
@@ -8,6 +9,7 @@ int numB = Integer.parseInt(str2)
 int counter = 0
 int product = 0
 
+//code multiplies the two numbers together without using the * operator
 while (counter < numB) {
 	product = product + numA
 	counter = counter + 1
