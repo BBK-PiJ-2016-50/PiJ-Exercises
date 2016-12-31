c7cf261bbb5c0d8e67e903294f8f201d577de47c
IanRobinson01 <ianrobinson01@hotmail.com>
2016-12-31 16:44:44 +0000

    added comments and removed some methods which were appropriate only for tasks 5 and 6 for week 5 task 4

--- Week 5/Task04/Matrix.java
+++ Week 5/Task04/Matrix.java
@@ -4,6 +4,7 @@ public class Matrix {
 	private int rows;
 	private int cols;
 	
+	//constructor setting the size of the array
 	public Matrix(int rows, int cols) {
 		this.rows = rows - 1;
 		this.cols = cols - 1;
@@ -15,6 +16,7 @@ public class Matrix {
 		}		
 	}
 	
+	//modify one element of the array
 	public void setElement(int row, int col, int newVal) {
 		if ((row <= this.rows && row >= 0) || (col <= this.cols && col >= 0)) {
 			matrix[row][col] = newVal;
@@ -23,6 +25,7 @@ public class Matrix {
 		}
 	}
 	
+	//modifies one whole row of the array
 	public void setRow(int row, String numList) {
 		int[] nums = stringToArray(numList);
 		if ((row <= this.rows && row >= 0) && (nums.length - 1 == this.cols)) {
@@ -34,6 +37,7 @@ public class Matrix {
 		}
 	}
 	
+	//modifies one whole column of the array
 	public void setColumn(int col, String numList) {
 		int[] nums = stringToArray(numList);
 		if ((col <= this.cols && col >= 0) && (nums.length - 1 == this.rows)) {
@@ -45,6 +49,7 @@ public class Matrix {
 		}
 	}
 	
+	//converts a string to an array
 	private int[] stringToArray(String arrayStr) {
 		String[] strNums = arrayStr.split(",");
 		int[] nums = new int[strNums.length];
@@ -53,7 +58,8 @@ public class Matrix {
 		}
 		return nums;
 	}
-
+	
+	//returns the values in the array as a String
 	public String toString() {
 		String strArray = "[";
 		for (int i = 0; i <= rows; i++) {
@@ -68,6 +74,7 @@ public class Matrix {
 		return strArray;
 	}
 	
+	//prints the values of the matrix on screen in a legible format
 	public void prettyPrint() {
 		for (int i = 0; i <= rows; i++) {
 			for (int j = 0; j <= cols; j++) {
@@ -75,18 +82,6 @@ public class Matrix {
 			}
 			System.out.println("");
 		}
-	}
-	
-	public boolean isSymmetrical() {
-		MatrixChecker mc = new MatrixChecker();
-		boolean isSym = mc.isSymmetrical2D(matrix);
-		return isSym;
-	}
-	
-	public boolean isTriangular() {
-		MatrixChecker mc = new MatrixChecker();
-		boolean isTri = mc.isTriangular(matrix);
-		return isTri;
 	}
 	
 }
