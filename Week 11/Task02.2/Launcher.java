065108323dea9da078ac9fc663693d1a7ba39bad
IanRobinson01 <ianrobinson01@hotmail.com>
2017-01-03 21:39:46 +0000

    completed solution for week 11 task 2.2

---
+++ Week 11/Task02.2/Launcher.java
@@ -0,0 +1,26 @@
+public class Launcher {
+	
+	public static void main(String[] args) {
+		Launcher script = new Launcher();
+		script.launch();
+	}
+	
+	public void launch() {
+		TreeIntSet tree = new TreeIntSet(2);
+		tree.add(3);
+		tree.add(4);
+		tree.add(5);
+		tree.add(6);
+		tree.add(2);
+		tree.add(9);
+		tree.add(3);
+		
+		System.out.println(tree.toString());
+		System.out.println(tree.contains(3));
+		System.out.println(tree.contains(100));
+		
+		tree.containsVerbose(9);
+		
+	}
+	
+}
