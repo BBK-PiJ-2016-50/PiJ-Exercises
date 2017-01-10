ba2b5d90aa4f1b5bc198573bc240866baf458c18
IanRobinson01 <ianrobinson01@hotmail.com>
2017-01-10 20:46:10 +0000

    added battery of tests to test hash utility class

--- Week 12/Task02/HashUtilitiesTest.java
+++ Week 12/Task02/HashUtilitiesTest.java
@@ -4,12 +4,52 @@ import static org.junit.Assert.*;
 public class HashUtilitiesTest {
 	
 	@Test
-	public void testsShortHash() {
-		String str = "dsgfdjdhj";
-		int hash = str.hashCode();
-		int shortHash = HashUtilities.shortHash(hash);
-		assertEquals(50, shortHash);
+	public void testShortHashWorks() {
+		int num = 436546;
+		int shortHash = HashUtilities.shortHash(num);
+		assertTrue(shortHash < 1000);
+	}
+	
+	@Test
+	public void testShortHashAbove() {
+		int num = 1001;
+		int shortHash = HashUtilities.shortHash(num);
+		assertEquals(1, shortHash);
+	}
+	
+	@Test
+	public void testShortHashBelow() {
+		int num = 999;
+		int shortHash = HashUtilities.shortHash(num);
+		assertEquals(999, shortHash);
+	}
 
+	@Test
+	public void testShortHashEqual() {
+		int num = 1000;
+		int shortHash = HashUtilities.shortHash(num);
+		assertEquals(0, shortHash);
+	}
+	
+	@Test
+	public void testShortHashZero() {
+		int num = 0;
+		int shortHash = HashUtilities.shortHash(num);
+		assertEquals(0, shortHash);
+	}
+	
+	@Test
+	public void testShortHashMassive() {
+		int num = 999934243;
+		int shortHash = HashUtilities.shortHash(num);
+		assertEquals(243, shortHash);
+	}
+	
+	@Test
+	public void testShortHashNegative() {
+		int num = -34673;
+		int shortHash = HashUtilities.shortHash(num);
+		assertEquals(673, shortHash);
 	}
 	
 }
