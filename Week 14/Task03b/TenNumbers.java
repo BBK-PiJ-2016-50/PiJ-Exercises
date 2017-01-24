59ee1c1eb4e9c21f2cd929470922780a8ae51b32
IanRobinson01 <ianrobinson01@hotmail.com>
2017-01-24 21:26:43 +0000

    first few exercises for week 14

---
+++ Week 14/Task03b/TenNumbers.java
@@ -0,0 +1,26 @@
+public class TenNumbers {
+
+  public static void main(String[] args) {
+    TenNumbers tn = new TenNumbers();
+	tn.launch();
+  }
+  
+  private void launch() {
+    String input = System.console().readLine("How many numbers?");
+    int counter = 0;
+	int total = 0;
+	while(counter < 10) {
+	  try {
+	    String input = System.console().readLine();
+	    int num = Integer.parseInt(input);
+		counter++;
+		total += num;
+	  } catch (NumberFormatException ex) {
+	    System.out.println("Not a number.  Try again...");
+	  }
+	}
+	int mean = total / counter;
+	System.out.println("The mean of the 10 numbers is " + mean);
+  }
+
+}
