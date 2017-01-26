f5180523dfdd3a10c6fcc53d4a0a0e0c29bf1c0c
IanRobinson01 <ianrobinson01@hotmail.com>
2017-01-26 23:03:38 +0000

    completed solution for week 14 task 6

--- Week 14/Task06/ExceptionThrower.java
+++ Week 14/Task06/ExceptionThrower.java
@@ -1,16 +1,31 @@
 public class ExceptionThrower {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws MyCheckedException {
 		ExceptionThrower exThrower = new ExceptionThrower();
-		exThrower.launch();
+		int input = Integer.parseInt(args[0]);
+		exThrower.launch(input);
 	}
 	
-	private void launch() {
-		//take input from user - 0-5, 6-10, 11-15, 16-20
-		//MyCheckedException - inside try block (I am a checked exception and i have been thrown inside a try block)
-		//MyCheckedException - outside try block, throws (I am a checked exception and i have been thrown outside a try block)
-		//MyRuntimeException - inside try block (I am a runtime exception and i have been thrown inside a try block)
-		//MyRuntimeException - outside try block, throws (I am a runtime exception and i have been thrown outside a try block)
+	private void launch(int input) throws MyCheckedException {
+		if (input == 1) {
+			try {
+				int arr[] = {1, 2, 3};
+				int num = arr[3];
+			} catch (IndexOutOfBoundsException ex) {
+				throw new MyCheckedException("I am a checked exception and i have been thrown inside a try block");
+			}
+		} else if (input == 2) {
+			try {
+			  int arr[] = {1, 2, 3};
+				int num = arr[3];
+			} catch (IndexOutOfBoundsException ex) {
+				throw new MyRuntimeException("I am a runtime exception and i have been thrown inside a try block");
+			}
+		} else if (input == 3) {
+			throw new MyCheckedException("I am a checked exception and i have been thrown outside a try block");
+		} else {
+			throw new MyRuntimeException("I am a runtime exception and i have been thrown outside a try block");
+		}
 	}
 	
 }
