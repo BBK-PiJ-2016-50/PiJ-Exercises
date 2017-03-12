f86e7be7fd5addc0f876bb28665fd4699d3b817e
IanRobinson01 <ianrobinson01@hotmail.com>
2017-03-12 20:37:45 +0000

    completed week 20 task 2

---
+++ Week 20/task02.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+public class task02 {
+
+ public static void main(String[] args) {
+   task02 t = new task02();
+   t.launch();
+ }
+
+ private void launch() {
+   String[] strArray = {"orange", "apple", "passion fruit", "pear", "banana"};
+   
+   //sort array by length
+   Arrays.sort(strArray, (w1, w2) -> w1.length() - w2.length());
+   System.out.println("Sorted by length (shortest to longest): " + Arrays.asList(strArray));
+   
+   //sort by reverse length
+   Arrays.sort(strArray, (w1, w2) -> w2.length() - w1.length());
+   System.out.println("Sorted by length (longest to shortest): " + Arrays.asList(strArray));
+   
+   //sort by first character
+   Arrays.sort(strArray, (w1, w2) -> w1.charAt(0) - w2.charAt(0));
+   System.out.println("Sorted by first letter: " + Arrays.asList(strArray));
+   
+   //strings containing e come first
+   Arrays.sort(strArray, task02::eChecker);
+   System.out.println("Sorted by words with e first: " + Arrays.asList(strArray));
+
+ }
+ 
+ private static int eChecker(String w1, String w2) {
+   if (w1.contains("e") && !w2.contains("e")) {
+       return -1;
+     } else if (!w1.contains("e") && w2.contains("e")) {
+       return 1;
+     } else {
+       return 0;
+     }
+ }
+
+}
